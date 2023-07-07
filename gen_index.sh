5fa56922c1f75a01599ce72411af01dfe91661a6
aturgano@gmail.com <aturgano@gmail.com>
2023-07-07 20:02:54 +0600

    change name index

--- gen_index.sh
+++ gen_index.sh
@@ -6,10 +6,10 @@ echo '        <meta charset="UTF-8">'
 echo '        <title>NGINX</title>'
 echo '    </head>'
 echo '    <body>'
-echo '        <h2>Дипломная работа</h2>'
+echo '        <h2>Дипломный практикум в Yandex.Cloud</h2>'
 echo '        <img src="main.jpg" name="Велосипеды" align="center" width="600" height="350"  border="0"/>' 
-echo '        <h4>Автор: Турганов Артем</h4>'
-echo "        <h4>$1</h4>"
+echo '        <h5>Автор: Турганов Артем</h5>'
+echo "        <h5>$1</h5>"
 echo '    </body>'
 echo '    </html>'
 }> ./nginx/index.html
