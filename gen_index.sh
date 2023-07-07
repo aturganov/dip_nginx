50d4988ad3fcb2c840ce9c364571edf146322f2e
aturgano@gmail.com <aturgano@gmail.com>
2023-07-07 20:16:06 +0600

    version

--- gen_index.sh
+++ gen_index.sh
@@ -9,7 +9,7 @@ echo '    <body>'
 echo '        <h2>Дипломный практикум в Yandex.Cloud</h2>'
 echo '        <img src="main.jpg" name="Велосипеды" align="center" width="600" height="350"  border="0"/>' 
 echo '        <h5>Автор: Турганов Артем</h5>'
-echo "        <h5>version: $1</h5>"
+echo "        <h5>Version: $1</h5>"
 echo '    </body>'
 echo '    </html>'
 }> ./nginx/index.html
